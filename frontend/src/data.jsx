fffc24d0d5c266b2579f8e1b0ed26b932484bf56
Gaurav Kolhe <kolheg58@gmail.com>
2024-03-10 12:59:22 +0530

    Movie Names and Bg color

--- frontend/src/data.jsx
+++ frontend/src/data.jsx
@@ -1,9 +1,9 @@
 export let moviesList = [
-    "Avengers: Endgame ",
-    "Titanic",
-    "3 Idiots",
-    "RRR",
-    "Harry Potter and the Chamber of Secrets ",
+    "Suraj par mangal bhari ",
+    "Tenet",
+    "The war with grandpa",
+    "The personal history of David Copperfield",
+    "Come play",
   ];
   export let slots = ["10:00 AM", "01:00 PM", "03:00 PM", "08:00 PM"];
   export let seats = ["A1", "A2", "A3", "A4", "D1", "D2"];
